50c2feb5b90fd6e421b43194c64bf3a8a8793079
Jefferson <jedoljak@gmail.com>
2018-08-11 23:25:25 -0300

    interface client && book

---
+++ src/app/book/book.module.ts
@@ -0,0 +1,10 @@
+interface Book {
+    id: number
+    title: string
+    author: string
+    yearPublished: number
+    price: string
+    rating: string
+}
+
+export { Book }
---
+++ src/app/client/client.module.ts
@@ -0,0 +1,11 @@
+interface Client{
+    id: number
+    fullName: string
+    gender: string
+    age: number
+    email: string
+    phone: string
+    username: string
+}
+
+export { Client }
